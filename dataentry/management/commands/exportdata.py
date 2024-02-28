21a80d62ec930fe6a699ab44f3f1b30c4d56d194
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-02-28 12:32:30 -0500

    Next Set of Changes

--- dataentry/management/commands/exportdata.py
+++ dataentry/management/commands/exportdata.py
@@ -1,31 +1,51 @@
-from django.core.management.base import BaseCommand
 import csv
-from dataentry.models import Student, Customer
+from django.core.management.base import BaseCommand
+from django.apps import apps
 import datetime
 
+
+# propsed command = python manage.py exportdata model_name
 class Command(BaseCommand):
-    help = 'Command to Export Data from Student Table to CSV Database'
+    help = 'Export data from the database to a CSV file'
 
-    def handle(self,*args,**kwargs):
-        ## first retrieve the data from database
-        students = Student.objects.all()
-  
-        ## Define the csv file and filepath
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        filepath = f'exported_data_data_{timestamp}.csv'
+    def add_arguments(self, parser):
+        parser.add_argument('model_name', type=str, help='Model name')
 
-        ## Open the csv file and then save the data
-        with open(filepath,'w',newline='') as file:
+    def handle(self, *args, **kwargs):
+        model_name = kwargs['model_name'].capitalize()
+
+        # search through all the installed apps for the model
+        model = None
+        for app_config in apps.get_app_configs():
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break # stop executing once the model is found
+            except LookupError:
+                pass
+        
+        if not model:
+            self.stderr.write(f'Model {model_name} cound not found')
+            return
+        
+        # fetch the data from the database
+        data = model.objects.all()
+
+        # generate csv file path
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        
+        file_path = f'exported_{model_name}_data_{timestamp}.csv'
+        print(file_path)
+
+        # open the csv file and write the data
+        with open(file_path, 'w', newline='') as file:
             writer = csv.writer(file)
 
-            ## write the csv header
-            writer.writerow(['Roll No', 'Name', 'Age'])
+            # write the CSV header
+            # we want to print the field names of the model that we are trying to export
+            writer.writerow([field.name for field in model._meta.fields])
 
-            #write data rows
-            for student in students:
-                writer.writerow([student.roll_no,student.name,student.age])
-
-        self.stdout.write(self.style.SUCCESS('Data Exported Successfully!'))
-
+            # write data rows
+            for dt in data:
+                writer.writerow([getattr(dt, field.name) for field in model._meta.fields])
         
-
+        self.stdout.write(self.style.SUCCESS('Data exported successfully!'))
