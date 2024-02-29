a4462f67f5b55a47928b384ab7f576abbb3de5c4
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-02-29 12:13:37 -0500

    Next Set of changes - 29th feb 1st sesssion

---
+++ dataentry/utils.py
@@ -0,0 +1,10 @@
+from django.apps import apps
+
+def get_all_custom_models():
+    default_models = ['Session', 'ContentType','LogEntry','Group', 'Permission', 'User']
+    custom_models = []
+
+    for model in apps.get_models():
+        if model.__name__ not in default_models:
+            custom_models.append(model.__name__)
+    return custom_models
--- dataentry/views.py
+++ dataentry/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from dataentry.utils import get_all_custom_models
+
 # Create your views here.
+def import_data(request):
+    if request.method == 'POST':
+        return
+    else:
+        custom_models = get_all_custom_models()
+        context = {
+            'custom_models': custom_models,
+        }
+
+    return render(request,'dataentry/importdata.html',context=context)
