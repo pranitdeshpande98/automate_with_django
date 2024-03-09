c24f6a9105bcf1426e19993ebc36ccd5148c2b41
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-03-09 17:49:21 -0500

    buik Email Send Model Creation

--- emails/views.py
+++ emails/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import redirect, render
+
+from emails.models import Subscriber
 from . forms import EmailForm
 from django.contrib import messages
+from dataentry.utils import send_email_notification
+from django.conf import settings
 # Create your views here.
 def send_email(request):
     if request.method == 'POST':
         email_form = EmailForm(request.POST, request.FILES)
         if email_form.is_valid():
-            email_form.save()
-
+            email_form = email_form.save()
             ## Sending an email
+            mail_subject = request.POST.get('subject')
+            message = request.POST.get('body')
+            email_list = request.POST.get('email_list')
+            ## Access the list
+            email_list = email_form.email_list
 
+            ## Extract Email Addresses from the subscriber model from accessed abovelist
+            subscribers = Subscriber.objects.filter(email_list=email_list)
+            to_email = [email.email_address for email in subscribers]
+
+            send_email_notification(mail_subject, message, to_email)
             ## Display a success message
             messages.success(request,'Email Send Successfully')
             return redirect('send_email')
