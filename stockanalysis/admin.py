156dc8af1badb2d486a41d020f8741ba6279a20c
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-03-13 16:49:06 -0400

    Next set of changes

--- stockanalysis/admin.py
+++ stockanalysis/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from . models import Stock, StockData
 
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('id','name','symbol')
+
 # Register your models here.
-admin.site.register(Stock)
+admin.site.register(Stock, StockAdmin)
 admin.site.register(StockData)
--- stockanalysis/urls.py
+++ stockanalysis/urls.py
@@ -7,4 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path('stocks/',views.stocks,name='stocks'),
     path('stock-autocomplete/', views.StockAutocomplete.as_view(),name="stock_autocomplete"),
+    path('stock-detail/<int:pk>/', views.stock_detail, name='stock_detail'),
 ]
