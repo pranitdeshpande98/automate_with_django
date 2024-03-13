b266c36b273514042e1220306522da91c5a40094
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-03-13 12:08:24 -0400

    Django Light form Autocomplete Functionality for Stock Forms

--- stockanalysis/forms.py
+++ stockanalysis/forms.py
@@ -3,5 +3,5 @@ from . models import Stock
 from django import forms
 
 class StockForm(forms.Form):
-    stock = forms.ModelChoiceField(queryset=Stock.objects.all(), widget=autocomplete.ModelSelect2(url='stock_autocomplete', ))
+    stock = forms.ModelChoiceField(queryset=Stock.objects.all(), widget=autocomplete.ModelSelect2(url='stock_autocomplete', attrs={'data-placeholder': 'Select a stock...'}))
 
